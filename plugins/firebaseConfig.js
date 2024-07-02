b554d58ee6d8bb6097eb5a2d4bcbf5b0bccd49a0
= <=>
2024-07-02 11:13:36 +0200

    config firebase changée

--- plugins/firebaseConfig.js
+++ plugins/firebaseConfig.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 // Remplacez ces valeurs par celles de votre configuration Firebase
 export default defineNuxtPlugin((nuxtApp) => {
     const firebaseConfig = {
-        apiKey: "AIzaSyAdIom0qlw9C5B3BfLJ2-uhBEX_KMxYqAs ",
-        authDomain: "wekanscape.web.app",
-        projectId: "wekanscape",
-        storageBucket: "gs://wekanscape.appspot.com",
-        messagingSenderId: "803651484787",
-        appId: "1:803651484787:web:736c6d72fac9994ae77e06"
+        apiKey: "AIzaSyCcC8CrT_hX9V-p69oVI1Q6VcpeB2WGvTc",
+        authDomain: "wecanscape-8ce7e.firebaseapp.com",
+        projectId: "wecanscape-8ce7e",
+        storageBucket: "wecanscape-8ce7e.appspot.com",
+        messagingSenderId: "646211129427",
+        appId: "1:646211129427:web:a09496cbe56e7cc1daf6a6"
     };
     const app = initializeApp(firebaseConfig)
     const auth = getAuth(app)
